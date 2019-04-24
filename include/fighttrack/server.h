7a9c6862a9372acffeb61ac83a6d0a739c9e47b8
Natanael Rabello <natanael.rabello.cwi@datacom.ind.br>
2019-04-24 17:29:46 -0300

    WIP: Classifying the Server

--- include/fighttrack/server.h
+++ include/fighttrack/server.h
@@ -1,14 +1,100 @@
 /**
  * \file server.h
- * \brief Server.
+ * \brief Server declearation.
  */
 
 #include <cstdint>
+#include <map>
+#include <deque>
+#include <thread>
+#include <mutex>
+#include <queue>
+
+#include <netinet/in.h>
 
 /**************************************************************************************/
 
 namespace fighttrack {
 
-int server(const uint16_t port);
+class Server {
+   public:
+    /**
+     * \brief Construct a new Server object>
+     */
+    Server();
+
+    /**
+     * \brief Destroy the Server object.
+     */
+    ~Server() = default;
+
+    /**
+     * \brief Create and configure the server socket.
+     * \param port Server port.
+     * \return 0 on sucess, negative if error.
+     */
+    int Initialize(const uint16_t port);
+
+    /**
+     * \brief Close all open connections and clean resources.
+     * \return 0 on sucess, negative if error.
+     */
+    int Terminate();
+
+    /* Client's received data */
+    struct ClientData {
+        int id;
+        std::string buffer;
+    };
+
+    /**
+     * \brief  Get queue of received messages from clients.
+     * \return Queue of messages. Might be empty if there are no new messages.
+     */
+    std::queue<ClientData> GetRxQueue();
+
+    /**
+     * \brief  Send data to clients.
+     * \param  client
+     * \return 0 on succes, negative if error.
+     */
+    int Transmit(ClientData client_data);
+
+   private:
+    /**
+     * \brief Thread runnable; Handle events of new connections and clients rx/tx.
+     */
+    void EventHandler();
+
+    /* TODO */
+    void AddNewClient();
+    void Received();
+
+    /* Client connection information */
+    struct ClientInfo {
+        int sock;
+        struct sockaddr_in addr;
+    };
+
+    //! Flag indicating if socket is initialized
+    bool initialized_;
+    //! Master socket which listens for new connections
+    int listen_sock_;
+    //! Event poll file descriptor
+    int epoll_fd_;
+    //! File descriptor used for notifying the event handling thread
+    int thread_event_fd_;
+    //! Event handling thread
+    std::thread listener_thread_;
+    //! Mutex to protect read/write from/to private members
+    std::mutex mutex_;
+    //! Queue of incomming messages from clients
+    std::queue<ClientData> rx_queue_;
+
+    //! Deque of available client IDs (sorted)
+    std::deque<int> client_ids_;
+    //! Map of clients. Key: client ID, Element: client Info
+    std::map<int, ClientInfo> clients_;
+};
 
 } /* namespace fighttrack */
--- src/server.cc
+++ src/server.cc
@@ -11,20 +11,19 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/eventfd.h>
 #include <sys/epoll.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 
-#include <map>
-#include <deque>
 #include <utility>
 #include <gsl/gsl>
 
 /**************************************************************************************/
 
-/** Helper function to transform an interger sequence into a initializer list */
-template<class T, T... I>
+/** Helper function to transform an integer sequence into a initializer list */
+template <class T, T... I>
 static constexpr auto make_initializer_list(std::integer_sequence<T, I...>)
 {
     return std::initializer_list<T>{ I... };
@@ -38,19 +37,34 @@ static constexpr size_t kMaxClients = 4;
 
 /**************************************************************************************/
 
-int server(uint16_t port)
+Server::Server()
+    : initialized_{ false },
+      listen_sock_{ 0 },
+      epoll_fd_{ 0 },
+      thread_event_fd_{ 0 },
+      listener_thread_{},
+      mutex_{},
+      rx_queue_{},
+      client_ids_{ make_initializer_list(std::make_integer_sequence<int, kMaxClients>{}) },
+      clients_{}
 {
-    int err = 0;
+}
 
-    auto _server_finished = gsl::finally([] { printf("Server finished\n"); });
+int Server::Initialize(uint16_t port)
+{
+    int ret = 0;
+    int err = 0;
 
     /* Create master socket */
     int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (listen_sock == -1) {
         perror("Failed to create socket");
-        return -1;
+        return ret = -1;
     }
-    auto _close_listen_socket = gsl::finally([&] { close(listen_sock); });
+    auto _close_listen_socket = gsl::finally([&] {
+        if (ret != 0)
+            close(listen_sock);
+    });
 
     { /* Make file descriptor non-blocking */
         int flags = fcntl(listen_sock, F_GETFL);
@@ -58,7 +72,7 @@ int server(uint16_t port)
         err = fcntl(listen_sock, F_SETFL, flags);
         if (err == -1) {
             perror("Failed to set socket control flags");
-            return -1;
+            return ret = -1;
         }
     }
 
@@ -67,7 +81,7 @@ int server(uint16_t port)
         err = setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(int));
         if (err == -1) {
             perror("Failed to set socket options");
-            return -1;
+            return ret = -1;
         }
     }
 
@@ -77,28 +91,35 @@ int server(uint16_t port)
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(port);
 
-    err = bind(listen_sock, (struct sockaddr*) &server_addr, sizeof(server_addr));
+    err = bind(listen_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
     if (err == -1) {
         perror("Failed to bind socket");
-        return -1;
+        return ret = -1;
     }
 
     err = listen(listen_sock, kMaxClients);
     if (err == -1) {
         perror("Failed to listen socket");
-        return -1;
+        return ret = -1;
+    }
+
+    /* Create a event file descriptor for threads communication */
+    thread_event_fd_ = eventfd(0, EFD_NONBLOCK);
+    if (thread_event_fd_ == -1) {
+        perror("Failed to create thread event fd");
+        return ret = -1;
     }
 
     /* Create the event poll */
     int epoll_fd = epoll_create1(0);
     if (epoll_fd == -1) {
         perror("Failed to create epoll");
-        return -1;
+        return ret = -1;
     }
-    auto _close_epoll_fd = gsl::finally([&] { close(epoll_fd); });
-
-    constexpr size_t kMaxEvents = 10;
-    struct epoll_event events[kMaxEvents];
+    auto _close_epoll_fd = gsl::finally([&] {
+        if (ret != 0)
+            close(epoll_fd);
+    });
 
     { /* Add master socket to event poll */
         struct epoll_event event;
@@ -107,94 +128,143 @@ int server(uint16_t port)
         err = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_sock, &event);
         if (err == -1) {
             perror("Failed to add socket to epoll");
-            return -1;
+            return ret = -1;
         }
     }
 
-    /* Client connection information */
-    struct client_info {
-        int sock;
-        struct sockaddr_in addr;
-    };
-
-    //! Deque of available client IDs (sorted)
-    std::deque<int> client_ids =
-        make_initializer_list(std::make_integer_sequence<int, kMaxClients>{});
-    //! Map from ID to client Info
-    std::map<int, struct client_info> clients;
-
-    auto _close_client_socks = gsl::finally([&] {
-        for (auto& client : clients) {
-            close(client.second.sock);
+    { /* Add Thread Event FD to event poll */
+        struct epoll_event event;
+        event.events = EPOLLIN;
+        event.data.fd = thread_event_fd_;
+        err = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, thread_event_fd_, &event);
+        if (err == -1) {
+            perror("Failed to add thread event fd to epoll");
+            return ret = -1;
         }
-    });
+    }
 
-    /* Listen for Client events (new connections and incoming data) */
+    /* Server socket initialized */
+    initialized_ = true;
+    /* Create event listener thread */
+    listener_thread_ = std::thread(&Server::EventHandler, this);
+
+    return ret = 0;
+}
+
+/**************************************************************************************/
+
+int Server::Terminate()
+{
+    if (!initialized_)
+        return 0;
+
+    /* Trigger the event thread to terminate */
+    int event = 1;
+    if (write(thread_event_fd_, &event, 1) == -1) {
+        perror("Failed to send terminate signal to server event thread");
+        fflush(stderr);
+    }
+    listener_thread_.join();
+
+    /* Close sockets and FDs */
+    for (auto& client : clients_) {
+        close(client.second.sock);
+    }
+    close(epoll_fd_);
+    close(listen_sock_);
+
+    initialized_ = false;
+    return 0;
+}
+
+/**************************************************************************************/
+
+void Server::EventHandler()
+{
+    int err = 0;
+    constexpr size_t kMaxEvents = 10;
+    struct epoll_event events[kMaxEvents];
+
+    /* Listen for client events (new connections and incoming data) */
     while (true) {
-        printf("Polling for events..\n");
+        printf("Server: Polling for events..\n");
         fflush(stdout);
 
         /* Wait for an event from master socket or client sockets */
-        constexpr int kTimeoutMs = 10'000;
-        int event_num = epoll_wait(epoll_fd, events, sizeof(events), kTimeoutMs);
+        constexpr int kTimeoutMs = 1'000;
+        int event_num = epoll_wait(epoll_fd_, events, sizeof(events), kTimeoutMs);
         if (event_num == -1) {
-            printf("Failed polling events");
-            return -1;
-        }
-        else if (event_num == 0) {
+            perror("Failed polling events");
+            return;
+        } else if (event_num == 0) {
             printf("Epoll timeout\n");
             continue;
         }
 
         for (int e = 0; e < event_num; ++e) {
+            std::lock_guard<std::mutex> lock{ mutex_ };
+            /* Thread notifications */
+            if (events[e].data.fd == thread_event_fd_) {
+                int event;
+                int n = read(thread_event_fd_, &event, 1);
+                if (n != 1) {
+                    perror("Failed to read thread event code");
+                    return;
+                }
+                /* Signal to terminate thread */
+                if (event == 1) {
+                    return;  // terminate
+                } else {
+                    fprintf(stderr, "Unknown thread event code received: %d, ignoring.\n", event);
+                    continue;
+                }
+            }
             /* New client event */
-            if (events[e].data.fd == listen_sock) {
-                if (clients.size() >= kMaxClients) {
+            else if (events[e].data.fd == listen_sock_) {
+                if (clients_.size() >= kMaxClients) {
                     printf("Dismissing new client, max reached.\n");
                     // There's no way to refuse directly, so accept and close immediatly
                     struct sockaddr client_addr;
                     socklen_t client_len;
-                    int client_sock = accept(listen_sock, &client_addr, &client_len);
+                    int client_sock = accept(listen_sock_, &client_addr, &client_len);
                     if (client_sock != -1) {
                         close(client_sock);
                     }
                     continue;
                 }
 
-                const int client_id = client_ids.front();
-                struct client_info client;
+                const int client_id = client_ids_.front();
+                ClientInfo client;
                 socklen_t client_len;
-                client.sock =
-                    accept(listen_sock, (struct sockaddr*) &client.addr, &client_len);
+                client.sock = accept(listen_sock_, (struct sockaddr*)&client.addr, &client_len);
                 if (client.sock == -1) {
                     perror("Failed to accept connection");
                     return -1;
                 }
                 /* Cache new client */
-                clients[client_id] = client;
-                client_ids.pop_front();
+                clients_[client_id] = client;
+                client_ids_.pop_front();
 
                 /* Add client fd to event poll */
                 struct epoll_event event;
                 event.events = EPOLLIN;
                 event.data.fd = client.sock;
-                err = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client.sock, &event);
+                err = epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, client.sock, &event);
                 if (err == -1) {
                     perror("Failed to add client to epoll");
                     return -1;
                 }
 
-                printf("Server: got connection from %s port %d\n",
-                       inet_ntoa(client.addr.sin_addr), ntohs(client.addr.sin_port));
+                printf("Server: got connection from %s port %d\n", inet_ntoa(client.addr.sin_addr),
+                       ntohs(client.addr.sin_port));
 
                 /* Send Hello message */
                 char buffer[100];
-                size_t len =
-                    snprintf(buffer, sizeof(buffer),
-                             "Hello from server, you are the client %d", client_id + 1);
-                int n = send(client.sock, buffer, len, 0);
-                if (n == -1) {
-                    perror("Failed to send data to client");
+                size_t len = snprintf(buffer, sizeof(buffer),
+                                      "Hello from server, you are the client %d", client_id + 1);
+                err = Transmit({ .id = client_id, .buffer = buffer });
+                if (err == -1) {
+                    fprintf(stderr, "Failed to send hello-message to client\n");
                     return -1;
                 }
             }
@@ -208,23 +278,22 @@ int server(uint16_t port)
                 /* Check for connection closed */
                 if (n == 0) {
                     /* Search for client in cache */
-                    auto client_it = clients.begin();
-                    for (; client_it != clients.end(); ++client_it) {
+                    auto client_it = clients_.begin();
+                    for (; client_it != clients_.end(); ++client_it) {
                         if (client_it->second.sock == client_sock)
                             break;
                     }
-                    if (client_it == clients.end()) {
-                        fprintf(stderr, "Fatal error, client sock %d not cached",
-                                client_sock);
+                    if (client_it == clients_.end()) {
+                        fprintf(stderr, "Fatal error, client sock %d not cached\n", client_sock);
                         return -1;
                     }
                     /* Close socket */
                     close(client_sock);
-                    clients.erase(client_it);
+                    clients_.erase(client_it);
                     /* Restore client id */
                     auto client_id = client_it->first;
-                    client_ids.emplace_back(client_id);
-                    std::sort(client_ids.begin(), client_ids.end());
+                    client_ids_.emplace_back(client_id);
+                    std::sort(client_ids_.begin(), client_ids_.end());
 
                     printf("Client %d connection closed.\n", client_id + 1);
                 }
@@ -240,6 +309,37 @@ int server(uint16_t port)
                 }
             }
         }
+    }
+}
+
+/**************************************************************************************/
+
+std::queue<Server::ClientData> Server::GetRxQueue()
+{
+    std::lock_guard<std::mutex> lock{ mutex_ };
+    // Return rx queued data and clean internal queue
+    std::queue<ClientData> ret;
+    rx_queue_.swap(ret);
+    return std::move(ret);
+}
+
+/**************************************************************************************/
+
+int Server::Transmit(ClientData client_data)
+{
+    std::lock_guard<std::mutex> lock{ mutex_ };
+    auto client_it = clients_.find(client_data.id);
+    if (client_it == clients_.end()) {
+        fprintf(stderr, "Failed to send data to client %d: client id not found\n", client_data.id);
+        return -1;
+    }
+
+    auto& client_sock = client_it->second.sock;
+
+    int n = send(client_sock, client_data.buffer.data(), client_data.buffer.length(), 0);
+    if (n == -1) {
+        perror("Failed to send data to client");
+        return -1;
     }
 
     return 0;
